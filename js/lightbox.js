e87dda877775c4a8b136a9e65cbd7e06b34e4942
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-04-30 21:05:53 +0700

    Add navigation feature to lightbox
    This commit adds the navigation feature to the lightbox gallery, allowing users to navigate between images using previous and next buttons. The following changes were made:
    
    1. Added prev and next buttons to the lightbox in index.html
    2. Styled the prev and next buttons in style.css
    3. Added JavaScript functions to handle the navigation logic in lightbox.js
    
    Now, users can click on the left and right arrow buttons to view the previous or next image in the gallery.

--- js/lightbox.js
+++ js/lightbox.js
@@ -3,6 +3,8 @@ let lightbox = document.getElementById('lightbox');
 let lightboxContent = document.getElementsByClassName('lightbox-content')[0];
 let lightboxCaption = document.getElementsByClassName('lightbox-caption')[0];
 let close = document.getElementsByClassName('close')[0];
+let prev = document.getElementsByClassName('prev')[0];
+let next = document.getElementsByClassName('next')[0];
 
 function showLightbox(imageSrc, imageAlt) {
     lightbox.classList.remove('lightbox-hidden');
@@ -19,6 +21,40 @@ for (let i = 0; i < thumbnails.length; i++) {
 function hideLightbox() {
     lightbox.classList.add('lightbox-hidden');
 }
+
+function showNextImage() {
+    for (let i = 0; i < thumbnails.length; i++) {
+        if (thumbnails[i].src === lightboxContent.src) {
+            if (i === thumbnails.length - 1) {
+                showLightbox(thumbnails[0].src, thumbnails.alt);
+            } else {
+                showLightbox(thumbnails[i + 1].src, thumbnails[i + 1].alt);
+            }
+            break;
+        }
+    }
+}
+
+function showPrevImage() {
+    for (let i = 0; i < thumbnails.length; i++) {
+        if (thumbnails[i].src === lightboxContent.src) {
+            if (i === 0) {
+                showLightbox(thumbnails[thumbnails.length - 1].src, thumbnails[thumbnails.length - 1].alt);
+            } else {
+                showLightbox(thumbnails[i - 1].src, thumbnails[i - 1].alt);
+            }
+            break;
+        }
+    }
+}
+
+prev.addEventListener('click', function() {
+    showPrevImage();
+})
+
+next.addEventListener('click', function() {
+    showNextImage();
+})
 
 close.addEventListener('click', function(){
     hideLightbox();
